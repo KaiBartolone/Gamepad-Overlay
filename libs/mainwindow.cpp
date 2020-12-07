952b016c00170f52310ddecef8969e38a64d7ffb
KaiBartolone <kbartolo@stanford.edu>
2020-12-07 16:52:55 -0500

    overlay widget scales initially with mainwindow

--- libs/mainwindow.cpp
+++ libs/mainwindow.cpp
@@ -7,8 +7,14 @@
 
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "overlay.h"
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    setAttribute(Qt::WA_TranslucentBackground);
+
+    int size = qMin(this->size().height(), this->size().width()) / 3;
+    ui->gamepad->setFixedSize(size, size);
 }
