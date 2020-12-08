ebdfd8b548a4a64d393676731e4835217b8f1af4
KaiBartolone <kbartolo@stanford.edu>
2020-12-08 13:37:32 -0500

    window transparent and always on top

--- libs/mainwindow.cpp
+++ libs/mainwindow.cpp
@@ -8,13 +8,18 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "overlay.h"
+#include <Windows.h>
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
-    setAttribute(Qt::WA_TranslucentBackground);
 
-    int size = qMin(this->size().height(), this->size().width()) / 3;
+    // SetForegroundWindow((HWND)winId());
+    // setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    setAttribute(Qt::WA_TranslucentBackground);
+    setWindowFlags(Qt::FramelessWindowHint);
+    SetWindowPos((HWND)winId(), HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+
+    int size = qMin(this->size().height(), this->size().width()) / 2;
     ui->gamepad->setFixedSize(size, size);
 }
--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <QApplication>
 #include <iostream>
 
-int main(int argc, char* argv[])
+int WinMain(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    w.showFullScreen();
     return a.exec();
 }
